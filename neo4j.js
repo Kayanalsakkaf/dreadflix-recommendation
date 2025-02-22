9087fa75fe14c2194866b15684499bc532370dde
Kayan <kayanalsakkaf@outlook.com>
2025-02-22 00:20:41 +0000

    update instance

--- neo4j.js
+++ neo4j.js
@@ -3,7 +3,7 @@ const neo4j = require("neo4j-driver");
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+  neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
 );
 
 async function testConnection() {
